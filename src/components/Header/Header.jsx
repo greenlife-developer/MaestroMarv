c22d77f4f1d89ea0ed58781bc08f28d76720960d
Opeyemi <yemijoshua81@gmail.com>
2024-12-02 17:25:33 +0100

    adding mobile menu nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/images/logo.svg"
+import logo from "../../assets/images/logo.svg";
+import logo2 from "../../assets/images/mobile/logo_mobile.svg";
+import logo3 from "../../assets/images/mobile/logo_mobile_menu.svg";
+import cancel from "../../assets/images/mobile/cross_mobile_menu.svg";
+import menu from "../../assets/images/mobile/menu.svg";
 
 export default function Header() {
-  return ( 
-    <nav class="navigation-container">
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isNavBarVisible, setIsNavBarVisible] = useState(true);
+
+  const showNavBar = () => {
+    setIsNavBarVisible(!isNavBarVisible);
+  };
+
+  return (
+    <nav
+      class={
+        isNavBarVisible
+          ? "navigation-container nav_visible"
+          : "navigation-container"
+      }
+    >
       <div>
-        <img src={logo} alt="logo" />
+        <img className="web_logo" src={logo} alt="logo" />
+        {isNavBarVisible ? (
+          <div className="menu_logo_cancel">
+            <img
+              onClick={showNavBar}
+              className="mobile_logo"
+              src={logo3}
+              alt=""
+            />
+            <img src={cancel} alt="" />
+          </div>
+        ) : (
+          <img
+            onClick={showNavBar}
+            className="mobile_logo"
+            src={logo2}
+            alt=""
+          />
+        )}
       </div>
       <div class="navigation-items">
         <ul>
@@ -28,14 +63,21 @@ export default function Header() {
           </li>
         </ul>
       </div>
-      <div class="reglog">
-        <a class="reg" href="/">
-          Register
-        </a>
-        <a class="log" href="">
-          Login
-        </a>
-      </div>
+      {isLoggedIn ? (
+        <div>
+          <h1>Hello, Joshua</h1>
+        </div>
+      ) : (
+        <div class="reglog">
+          <a class="reg" href="/">
+            Register
+          </a>
+          <a class="log" href="">
+            Login
+          </a>
+        </div>
+      )}
+      <img onClick={showNavBar} className="menu_icon" src={menu} alt="" />
     </nav>
   );
 }
